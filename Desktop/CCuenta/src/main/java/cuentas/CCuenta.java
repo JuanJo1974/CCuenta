2ce04bbe245be81ba381b4f8df2cd4b4c7fb09a3
JuanJo1974 <74205271+JuanJo1974@users.noreply.github.com>
2023-02-11 12:52:54 +0100

    Primer Commit Netbeans.

---
+++ Desktop/CCuenta/src/main/java/cuentas/CCuenta.java
@@ -0,0 +1,124 @@
+package cuentas;
+
+/**
+ *
+ * @author jjose
+ */
+public class CCuenta {
+
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInterés;
+
+    /**
+     *
+     */
+    public CCuenta() {
+    }
+
+    /**
+     *
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo
+     */
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public double estado() {
+        return saldo;
+    }
+
+    /**
+     *
+     * @param cantidad
+     * @throws Exception
+     */
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0) {
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        }
+        saldo = saldo + cantidad;
+    }
+
+    /**
+     *
+     * @param cantidad
+     * @throws Exception
+     */
+    public void retirar(double cantidad) throws Exception {
+        if (cantidad <= 0) {
+            throw new Exception("No se puede retirar una cantidad negativa");
+        }
+        if (estado() < cantidad) {
+            throw new Exception("No se hay suficiente saldo");
+        }
+        saldo = saldo - cantidad;
+    }
+
+    /**
+     * @return the nombre
+     */
+    private String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    private void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the cuenta
+     */
+    private String getCuenta() {
+        return cuenta;
+    }
+
+    /**
+     * @param cuenta the cuenta to set
+     */
+    private void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * @return the saldo
+     */
+    private double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    private void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the tipoInterés
+     */
+    private double getTipoInterés() {
+        return tipoInterés;
+    }
+
+    /**
+     * @param tipoInterés the tipoInterés to set
+     */
+    private void setTipoInterés(double tipoInterés) {
+        this.tipoInterés = tipoInterés;
+    }
+
+}
